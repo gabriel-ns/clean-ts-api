18c3a9d12bafdd3d883a6a4f346542b583acea23
Gabriel Santos <gaabrielns@gmail.com>
2021-03-19 08:47:22 -0300

    feat: ensure mongodb reconnects if client is down

---
+++ src/infra/db/mongodb/helpers/mongo-helper.spec.ts
@@ -0,0 +1,28 @@
+import { MongoHelper as sut } from './mongo-helper'
+
+describe('Mongo Helper', () => {
+  beforeAll(async () => {
+    await sut.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await sut.disconnect()
+  })
+
+  test('Should return a collection if connected', async () => {
+    const accountCollection = sut.getCollection('accounts')
+    expect(accountCollection).toBeTruthy()
+  })
+
+  test('Should reconnect if mongodb is intentionally disconnected', async () => {
+    await sut.disconnect()
+    const accountCollection = await sut.getCollection('accounts')
+    expect(accountCollection).toBeTruthy()
+  })
+
+  test('Should reconnect if mongodb is accidentally disconnected', async () => {
+    await sut.client.close()
+    await sut.getCollection('accounts')
+    expect(sut.client.isConnected()).toBeTruthy()
+  })
+})
