007d52c96ef3bbe88e000d3756a847e31bb75003
Gabriel Santos <gaabrielns@gmail.com>
2021-04-12 17:57:46 -0300

    refactor: improve class constructors

--- src/presentation/controllers/login/login-controller.spec.ts
+++ src/presentation/controllers/login/login-controller.spec.ts
@@ -1,7 +1,7 @@
 import { Validation, HttpRequest, Authentication, AuthenticationModel } from './login-controller-protocols'
 import { MissingParamError } from '../../errors'
 import { badRequest, ok, serverError, unauthorized } from '../../helpers/http/http-helper'
-import { LoginController } from './login-login-controller'
+import { LoginController } from './login-controller'
 
 const makeAuthentication = (): Authentication => {
   class AuthenticationStub implements Authentication {
