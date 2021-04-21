812c8b1695cffb6780ec2bc99cacb6361873b760
Gabriel Santos <gaabrielns@gmail.com>
2021-04-21 18:03:46 -0300

    refactor: separate factories in folders and files

---
+++ src/main/factories/usecases/add-account/db-add-account-factory.ts
@@ -0,0 +1,12 @@
+import { AccountMongoRepository } from '../../../../infra/db/mongodb/account/account-mongo-repository'
+import { BcryptAdapter } from '../../../../infra/crypto/bcrypt-adapter'
+import { AddAccount } from '../../../../domain/usecases/add-account'
+import { DbAddAccount } from '../../../../data/usescases/add-account/db-add-account'
+
+export const makeDbAddAccount = (): AddAccount => {
+  const accountRepository = new AccountMongoRepository()
+  const salt = 12
+  const hasher = new BcryptAdapter(salt)
+  const addAccount = new DbAddAccount(hasher, accountRepository)
+  return addAccount
+}
