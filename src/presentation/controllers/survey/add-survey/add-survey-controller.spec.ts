2179470f17c14daf86cfa1e06d2670c806d17086
Gabriel Santos <gaabrielns@gmail.com>
2021-04-22 20:11:58 -0300

    test: ensure AddSurveyController returns a bad request error if validation fails

--- src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -1,7 +1,7 @@
 import { HttpRequest } from './add-survey-controller-protocols'
 import { AddSurveyController } from './add-survey-controller'
-import { Validation } from '../../../protocols'
-
+import { Controller, Validation } from '../../../protocols'
+import { badRequest } from '../../../helpers/http/http-helper'
 const makeFakeRequest = (): HttpRequest => ({
   body: {
     question: 'any_question',
@@ -21,13 +21,34 @@ const makeValidationStub = (): Validation => {
   return new ValidationStub()
 }
 
+interface SutTypes {
+  sut: Controller
+  validationStub: Validation
+}
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidationStub()
+  const sut = new AddSurveyController(validationStub)
+  return {
+    sut,
+    validationStub
+  }
+}
+
 describe('Add Survey Controller', () => {
   test('Should call validation with correct values', async () => {
-    const validationStub = makeValidationStub()
+    const { sut, validationStub } = makeSut()
     const validateSpy = jest.spyOn(validationStub, 'validate')
-    const sut = new AddSurveyController(validationStub)
     const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  test('Should return 400 if validation fails', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
+    const httpRequest = makeFakeRequest()
+    const response = await sut.handle(httpRequest)
+    expect(response).toEqual(badRequest(new Error()))
   })
 })
