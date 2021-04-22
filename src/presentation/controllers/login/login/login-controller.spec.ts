b94621f73dae01d2ad822e1e7ea07438e060168b
Gabriel Santos <gaabrielns@gmail.com>
2021-04-22 19:50:22 -0300

    refactor: move login and signup controllers to a login folder

--- src/presentation/controllers/login/login/login-controller.spec.ts
+++ src/presentation/controllers/login/login/login-controller.spec.ts
@@ -1,6 +1,6 @@
 import { Validation, HttpRequest, Authentication, AuthenticationModel } from './login-controller-protocols'
-import { MissingParamError } from '../../errors'
-import { badRequest, ok, serverError, unauthorized } from '../../helpers/http/http-helper'
+import { MissingParamError } from '../../../errors'
+import { badRequest, ok, serverError, unauthorized } from '../../../helpers/http/http-helper'
 import { LoginController } from './login-controller'
 
 const makeAuthentication = (): Authentication => {
--- src/presentation/controllers/login/signup/signup-controller.spec.ts
+++ src/presentation/controllers/login/signup/signup-controller.spec.ts
@@ -1,8 +1,8 @@
-import { EmailInUseError, MissingParamError, ServerError } from '../../errors'
+import { EmailInUseError, MissingParamError, ServerError } from '../../../errors'
 import { AddAccount, AddAccountModel, AccountModel, Validation, Authentication, AuthenticationModel } from './signup-controller-protocols'
 import { SignUpController } from './signup-controller'
-import { HttpRequest } from '../../protocols'
-import { ok, serverError, badRequest, forbidden } from '../../helpers/http/http-helper'
+import { HttpRequest } from '../../../protocols'
+import { ok, serverError, badRequest, forbidden } from '../../../helpers/http/http-helper'
 
 const makeAddAccount = (): AddAccount => {
   class AddAccountStub implements AddAccount {
