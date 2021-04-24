78aad46752430bf5a6d70857b9c9e1bc097d791f
Gabriel Santos <gaabrielns@gmail.com>
2021-04-24 10:06:31 -0300

    test: ensure DbLoadAccountByToken calls Decrypter with correct value

---
+++ src/data/usescases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -0,0 +1,35 @@
+import { DbLoadAccountByToken } from './db-load-account-by-token'
+import { Decrypter } from '../../protocols/crypto/decrypter'
+import { LoadAccountByToken } from '../../../domain/usecases/load-account-by-token'
+
+const makeDecrypterStub = (): Decrypter => {
+  class DecrypterStub implements Decrypter {
+    async decrypt (value: string): Promise<string> {
+      return new Promise(resolve => resolve('any_string'))
+    }
+  }
+  return new DecrypterStub()
+}
+
+interface SutTypes {
+  sut: LoadAccountByToken
+  decrypterStub: Decrypter
+}
+
+const makeSut = (): SutTypes => {
+  const decrypterStub = makeDecrypterStub()
+  const sut = new DbLoadAccountByToken(decrypterStub)
+  return {
+    sut,
+    decrypterStub
+  }
+}
+
+describe('DbLoadAccountByToken Usecase', () => {
+  test('Should call Decrypter with correct values', async () => {
+    const { sut, decrypterStub } = makeSut()
+    const decryptSpy = jest.spyOn(decrypterStub, 'decrypt')
+    await sut.load('any_token')
+    expect(decryptSpy).toHaveBeenCalledWith('any_token')
+  })
+})
