b903a75cd7a74156d880acc3dd5f70421959d2d3
Gabriel Santos <gaabrielns@gmail.com>
2021-04-25 23:58:41 -0300

    feat: ensure load surveys route returns 403 if no access token is provided

---
+++ src/main/factories/usecases/survey/load-surveys/db-load-surveys-factory.ts
@@ -0,0 +1,9 @@
+import { DbLoadSurveys } from '../../../../../data/usescases/load-surveys/db-load-surveys'
+import { LoadSurveys } from '../../../../../domain/usecases/load-surveys'
+import { SurveyMongoRepository } from '../../../../../infra/db/mongodb/survey/survey-mongo-repository'
+
+export const makeDbLoadSurveys = (): LoadSurveys => {
+  const surveyRepository = new SurveyMongoRepository()
+  const loadSurveys = new DbLoadSurveys(surveyRepository)
+  return loadSurveys
+}
