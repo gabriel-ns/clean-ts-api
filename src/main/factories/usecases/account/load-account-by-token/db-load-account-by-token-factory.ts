5abfb680ab50c5c78974e65bb240648e7f273f87
Gabriel Santos <gaabrielns@gmail.com>
2021-06-27 18:08:45 -0300

    chore: update references

--- src/main/factories/usecases/account/load-account-by-token/db-load-account-by-token-factory.ts
+++ src/main/factories/usecases/account/load-account-by-token/db-load-account-by-token-factory.ts
@@ -1,5 +1,5 @@
 import { AccountMongoRepository } from '@/infra/db/mongodb/account/account-mongo-repository'
-import { LoadAccountByToken } from '@/domain/usecases/load-account-by-token'
+import { LoadAccountByToken } from '@/domain/usecases/account/load-account-by-token'
 import { DbLoadAccountByToken } from '@/data/usescases/account/load-account-by-token/db-load-account-by-token'
 import { JwtAdapter } from '@/infra/crypto/jwt-adapter/jwt-adapter'
 import env from '@/main/config/env'
