5f00e9126ea80fd9da358699f4a2e854f33780d7
Gabriel Santos <gaabrielns@gmail.com>
2021-07-07 11:27:26 -0300

    feat: ensure DbLoadSurveyResult calls LoadSurveyResultRepository with correct param

--- src/domain/usecases/survey-result/load-survey-result.ts
+++ src/domain/usecases/survey-result/load-survey-result.ts
@@ -1,5 +1,5 @@
 import { SurveyResultModel } from '@/domain/models/survey-result'
 
-export interface LoadSurveyResults {
+export interface LoadSurveyResult {
   load (surveyId: string): Promise<SurveyResultModel>
 }
